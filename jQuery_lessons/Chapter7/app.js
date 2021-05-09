138bf5ab5bdcb1e6200a90d9c982746031148a2e
masaya-kawamura <lucky.number0128@gmail.com>
2021-05-09 20:14:22 +0900

    [Finish]Chapter7

---
+++ jQuery_lessons/Chapter7/app.js
@@ -0,0 +1,30 @@
+$(function(){
+  $('.box1').on('click',function(){
+    $(this).slideUp();
+  });
+});
+
+//上記コードにまとめる
+// $(function(){
+//   $('.bg1').on('click',function(){
+//     $('.bg1').slideUp();
+//   });
+// });
+
+// $(function(){
+//   $('.bg2').on('click',function(){
+//     $('.bg2').slideUp();
+//   });
+// });
+
+// $(function(){
+//   $('.bg3').on('click',function(){
+//     $('.bg3').slideUp();
+//   });
+// });
+
+// $(function(){
+//   $('.bg4').on('click',function(){
+//     $('.bg4').slideUp();
+//   });
+// });
